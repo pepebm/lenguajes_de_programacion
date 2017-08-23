5477d72ec535f866109f29b1866b45a4de5b148a
Pepe Beauregard <0jmbea0@Pepes-MacBook-Pro.local>
2017-08-23 13:05:08 -0500

    Recursion Tail

--- Practicas/Actividad1.hs
+++ Practicas/Actividad1.hs
@@ -4,10 +4,10 @@
 [(x,y,z)| x<-[1..100], y<-[1..100], z<-[1..100], z^2==x^2+y^2]
 
 -- Numero negativos
-[y|x<-[-1000..1000], let y = x<0]
+[y |x<-[-1000..1000], let y = x<0]
 
 -- La suma de todos los numeros impares abajo de 1000000
-sum[x|x<-[1..1000000], mod x 2 == 1]
+sum[x |x<-[1..1000000], mod x 2 == 1]
 
 -- Combinar un text en minúsculas, pasarlo a mayúsculas (import Data.Char)
 map(toUpper)"Hellaso"
@@ -23,4 +23,4 @@ factorial n = n * (factorial(n-1));
 
 -- Funcion que obtiene n ultimos elementos de una list
 let lastt n i list = lastt n-1 i+1
-lastt 
+lastt
